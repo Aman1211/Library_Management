e2d2f1e4db928bcd6f5e5058ba3f015aa34677e4
Maulik Jadav <maulik9211@gmail.com>
2020-12-22 15:38:09 +0530

    Conflicts resolved

--- src/library/dao/TransactiondaoService.java
+++ src/library/dao/TransactiondaoService.java
@@ -7,13 +7,9 @@ public abstract class TransactiondaoService {
   
 	public abstract void issue(TransactionTO tt);
 	public abstract TransactionTO fetchTransaction(String uname,String isbn);
-<<<<<<< HEAD
 	public abstract ArrayList<TransactionTO> totalAmoountOfFine(String date);
 	public abstract ArrayList<TransactionTO> top3user(String date);
-	
-=======
 	public abstract ArrayList<TransactionTO> fetchTransactions(String uname);
 	public abstract TransactionTO fetchTransaction(int id);
 	public abstract void markFine(int id,int fine);
->>>>>>> d297e29afbf2ac35750c5009c17de6005061a39d
 }
